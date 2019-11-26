416eb2490aa23c2b192efb598a99001b42ab7adc
Dennis Hendriksen <d.hendriksen@umcg.nl>
2019-11-26 14:43:04 +0100

    Automatically create groups for root packages

--- molgenis-data-validation/src/main/java/org/molgenis/data/validation/meta/model/PackageRepositoryDecoratorFactory.java
+++ molgenis-data-validation/src/main/java/org/molgenis/data/validation/meta/model/PackageRepositoryDecoratorFactory.java
@@ -5,9 +5,12 @@ import static java.util.Objects.requireNonNull;
 import org.molgenis.data.AbstractSystemRepositoryDecoratorFactory;
 import org.molgenis.data.DataService;
 import org.molgenis.data.Repository;
+import org.molgenis.data.meta.MetaDataService;
 import org.molgenis.data.meta.PackageRepositoryDecorator;
 import org.molgenis.data.meta.model.Package;
 import org.molgenis.data.meta.model.PackageMetadata;
+import org.molgenis.data.security.auth.GroupPackageRepositoryDecorator;
+import org.molgenis.data.security.auth.GroupPackageService;
 import org.molgenis.data.security.meta.PackageRepositorySecurityDecorator;
 import org.molgenis.data.validation.meta.PackageRepositoryValidationDecorator;
 import org.molgenis.data.validation.meta.PackageValidator;
@@ -23,23 +26,30 @@ public class PackageRepositoryDecoratorFactory
   private final PackageValidator packageValidator;
   private final MutableAclService mutableAclService;
   private final UserPermissionEvaluator userPermissionEvaluator;
+  private final GroupPackageService groupPackageService;
+  private final MetaDataService metadataService;
 
   public PackageRepositoryDecoratorFactory(
       PackageMetadata packageMetadata,
       DataService dataService,
       PackageValidator packageValidator,
       MutableAclService mutableAclService,
-      UserPermissionEvaluator userPermissionEvaluator) {
+      UserPermissionEvaluator userPermissionEvaluator,
+      GroupPackageService groupPackageService,
+      MetaDataService metadataService) {
     super(packageMetadata);
     this.dataService = requireNonNull(dataService);
     this.packageValidator = requireNonNull(packageValidator);
     this.mutableAclService = requireNonNull(mutableAclService);
     this.userPermissionEvaluator = requireNonNull(userPermissionEvaluator);
+    this.groupPackageService = requireNonNull(groupPackageService);
+    this.metadataService = requireNonNull(metadataService);
   }
 
   @Override
   public Repository<Package> createDecoratedRepository(Repository<Package> repository) {
     repository = new PackageRepositoryDecorator(repository, dataService);
+    repository = new GroupPackageRepositoryDecorator(repository, groupPackageService);
     repository =
         new PackageRepositorySecurityDecorator(
             repository, mutableAclService, userPermissionEvaluator);
